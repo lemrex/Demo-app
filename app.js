1afb0d01708445d0e637accd3595b33465e5656b
lemrex <emekaralf@gmail.com>
2023-09-15 04:32:06 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-import express from "express";
-import bodyParser from "body-parser";
+const express = require('express');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "I am using babel in NodeJS",
-    status: "success",
-  });
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 4200;
-app.listen(PORT, () => {
-  console.log("server up and running");
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
 });
---
+++ build/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
+//# sourceMappingURL=app.js.map
