b1d0816558242e3589c247a4699fc3c39b918125
lemrex <emekaralf@gmail.com>
2023-09-15 02:36:32 +0800

    God Help Me

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  resolve: {
-    fallback: {
-      zlib: require.resolve("browserify-zlib"),
-      crypto: require.resolve("browserify-crypto"),
-      path: require.resolve("path-browserify"),
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      http: require.resolve("stream-http"),
-      querystring: require.resolve("querystring-es3"), 
-      url: require.resolve("url/") ,
-      util: require.resolve("util/"),
-      fs
-    },
-      externals: {
-      express: require("express"),
-      on-finished: require("on-finished"),
-      raw-body: require("raw-body"),
-      browserify-zlib: require("browserify-zlib"),
-      destroy: require("destroy"),
-      etag:require("etag"),
-      mime:require("mime"),
-      send: require("send"),
-    },
-  },
- plugins: [
-    new webpack.ProvidePlugin({
-      fs: 'fs-extra',
-    })
-  ]
-  },
   entry: './src/server/index.js',
   output: {
     filename: 'bundle.js',
