3557123222422cb38e29c31c4c00038afba11f2c
lemrex <emekaralf@gmail.com>
2023-09-15 00:52:56 +0800

    adding polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,20 @@
 const path = require('path');
 
 module.exports = {
+  resolve: {
+    fallback: {
+      node-core-modules: require.resolve("node-core-modules"),
+      zlib: require.resolve("browserify-zlib"),
+      crypto: require.resolve("browserify-crypto"),
+      path: require.resolve("path-browserify"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      http: require.resolve("stream-http"),
+      querystring: require.resolve("querystring-es3"), 
+      url: require.resolve("url/") ,
+      util: require.resolve("util/"),
+    },
+  },
   entry: './src/server/index.js',
   output: {
     filename: 'bundle.js',
