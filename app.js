ff59c5599498be55e9f0ec2823b7516e23546d77
Emeka Raphael <95745431+lemrex@users.noreply.github.com>
2023-10-09 14:14:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Helloooo Word!');
+  res.send('Helloooo World!');
 });
 
 app.listen(3000, () => {
